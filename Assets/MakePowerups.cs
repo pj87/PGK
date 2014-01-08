6095295ce4f5e862cc2935b6fe742d6d62508999
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-08 14:38:09 +0100

    Automagiczne dodawanie powerup'ów.

---
+++ Assets/MakePowerups.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakePowerups : MonoBehaviour {
+
+    public Transform powerup; 
+
+	// Use this for initialization
+	void Start () {
+	    for (int z = 0; z < 5; z++) {
+            for (int x = 0; x < 5; x++)
+            {
+                var tmp = Instantiate(powerup, new Vector3(910 + x, 1.5f, 640 + z), Quaternion.identity) as Transform;
+            } 
+	    } 
+    } 
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
