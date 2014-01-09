d7c0334864915ca0c21bf0eaee8bf15ad8f9e6d3
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-09 23:57:40 +0100

    Dodany element GUI.

--- Assets/GUIScript.cs
+++ Assets/GUIScript.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine; 
+using System.Collections; 
 
-public class GUIScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+public class GUIScript : MonoBehaviour { 
+    public GUIStyle style; 
+    int life=123; 
+    void OnGUI() 
+    { 
+        GUILayout.BeginArea(new Rect(Screen.width-200, 0, 5000, 500)); 
+        //Tutaj można sobie forem wyświetlać jakieś fajne listy 
+        //GUILayout.BeginVertical 
+        GUILayout.Label("ŻYCIE: " + life, style); 
+        //Z boku pasek do przewijania 
+        //GUILayout.BeginScrollView 
+        GUILayout.EndArea(); 
+    } 
+} 
