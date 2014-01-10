efa61ed1e01dc8a4b1100202681d88d212a994b3
Pawel Jastrzebski <p.k.jastrzebski@gmail.com>
2014-01-10 15:15:56 +0100

    Dodany healthbar.

--- Assets/GUIScript.cs
+++ Assets/GUIScript.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class GUIScript : MonoBehaviour { 
     public GUIStyle style; 
-    int life=123; 
+    public float life = 93; 
     void OnGUI() 
-    { 
+    {
+        GUI.Box(new Rect(10, 10, Screen.width / 2 / (life / 100.0f), 25), life + "/" + 100); 
+
         GUILayout.BeginArea(new Rect(Screen.width-200, 0, 5000, 500)); 
         //Tutaj można sobie forem wyświetlać jakieś fajne listy 
         //GUILayout.BeginVertical 
